26a9cc674dc18dce4a337b1a39bf4caa4eecb6d3
s1f102302540 <s1f102302540@iniad.org>
2024-07-04 11:38:23 +0900

    some fix

--- todo/views.py
+++ todo/views.py
@@ -7,8 +7,8 @@ from todo.models import Task
 
 def index(request):
     if request.method == "POST":
-        task=Task(title=request.POST["title"], due_at=make_aware(parse_datetime(request.POST["due_at"])))
-
+        task=Task(title=request.POST["title"],
+                  due_at=make_aware(parse_datetime(request.POST["due_at"])))
         task.save()
 
     if request.GET.get("order") == "due":
@@ -16,7 +16,7 @@ def index(request):
     else:
         tasks=Task.objects.order_by("-posted_at")
 
-    tasks=Task.objects.all()
+
 
     context={
         "tasks":tasks
