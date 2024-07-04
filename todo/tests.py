90681e7b8cedbf7a5328e5e9ecf15b2b04495c2b
s1f102302540 <s1f102302540@iniad.org>
2024-07-04 11:56:03 +0900

    fixed bugs on flake8 (2)

--- todo/tests.py
+++ todo/tests.py
@@ -4,9 +4,11 @@ from datetime import datetime
 from todo.models import Task
 
 # Create your tests here.
+
 class SampleTestCase(TestCase):
     def test_sample1(self):
         self.assertEqual(1 + 2, 3)
+
 
 class TaskModelTestCase(TestCase):
     def test_create_task1(self):
@@ -35,7 +37,7 @@ class TaskModelTestCase(TestCase):
         task.save()
 
         self.assertFalse(task.is_overdue(current))
-    
+
     def test_is_overdue_past(self):
         due = timezone.make_aware(datetime(2024, 6, 30, 23, 59, 59))
         current = timezone.make_aware(datetime(2024, 7, 1, 0, 0, 0))
@@ -51,6 +53,7 @@ class TaskModelTestCase(TestCase):
 
         self.assertFalse(task.is_overdue(current))
 
+
 class TodoViewTestCase(TestCase):
     def test_index_get(self):
         client = Client()
@@ -59,7 +62,7 @@ class TodoViewTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.templates[0].name, "todo/index.html")
         self.assertEqual(len(response.context["tasks"]), 0)
-    
+
     def test_index_post(self):
         client = Client()
         data = {"title": "Test Task", "due_at": "2024-06-30 23:59:59"}
@@ -68,7 +71,7 @@ class TodoViewTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.templates[0].name, "todo/index.html")
         self.assertEqual(len(response.context["tasks"]), 1)
-    
+
     def test_index_get_order_post(self):
         task1 = Task(title="task1", due_at=timezone.make_aware(datetime(2024, 7, 1)))
         task1.save()
